0d843966656d609531b2de0547dab62172edccfe
JayN <jonimbalkar@berkeley.edu>
2026-02-28 13:59:59 -0800

    feat: implemented screen capture and conversion to base64

--- src/capture/index.js
+++ src/capture/index.js
@@ -1,5 +1,8 @@
-function captureScreen() {
-  console.log('captureScreen: not implemented');
+const screenshot = require('screenshot-desktop');
+
+async function captureScreen() {
+  const imgBuffer = await screenshot({ format: 'jpg' });
+  return imgBuffer.toString('base64');
 }
 
 module.exports = {
